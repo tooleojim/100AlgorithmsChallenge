b4f5818080b85c3e40003f06b3c93fde057c2b33
Jim OToole <tooleojim@gmail.com>
2019-08-29 02:09:53 +0100

    Solved

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,20 @@
 function add(param1: number, param2: number): number {
-
+    return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+function add2(param1: number[]): number {
+    var sum :number = 0;
+    var i : number;
+    for (i = 0 ; i < param1.length ; i++) 
+    {
+        sum += param1[i];
+    }
+    return sum;
+}
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2([1,2,3,4,5]));
+console.log(add2([2,3]));
 
