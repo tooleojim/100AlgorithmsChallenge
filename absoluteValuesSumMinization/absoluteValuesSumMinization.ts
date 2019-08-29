b76f9e2290a552ccf6b2cfeee061e02c0fe01a9a
Jim OToole <tooleojim@gmail.com>
2019-08-29 01:59:14 +0100

    Solved

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,26 @@
 function absoluteValuesSumMinimization(a: number[]): number {
 
+    var len : number = a.length;
+    var median : number;
+
+    if (len == 0)
+    {
+         median = null;
+    }
+    else
+    {
+        a.sort();
+
+        var middle : number = Math.floor((a.length - 1) / 2);
+
+        median = a[middle];
+    }
+
+    return median;
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
